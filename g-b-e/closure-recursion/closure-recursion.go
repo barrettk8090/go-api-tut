2f2d4089cf1e3c656118c6fd29a652d8017ca6d1
Barrett <barrettkowalsky@gmail.com>
2025-11-17 11:53:20 -0500

    closure-rec

---
+++ g-b-e/closure-recursion/closure-recursion.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
+func main() {
+	// closure
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInts := intSeq()
+	fmt.Println(newInts())
+
+	// recursion
+	fmt.Println(fact(7))
+
+	var fib func(n int) int
+
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+		return fib(n-1) + fib(n-2)
+	}
+
+	fmt.Println(fib(7))
+}
