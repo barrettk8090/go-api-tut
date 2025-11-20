04062514fa93a10ed04e7a323a2946a9d4a6bedb
Barrett <barrettkowalsky@gmail.com>
2025-11-20 09:38:05 -0500

    enums

---
+++ g-b-e/enums/enums.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type ServerState int
+
+const (
+	StateIdle ServerState = iota
+	StateConnected
+	StateError
+	StateRetrying
+)
+
+var stateName = map[ServerState]string{
+	StateIdle:      "idle",
+	StateConnected: "connected",
+	StateError:     "error",
+	StateRetrying:  "retrying",
+}
+
+func (ss ServerState) String() string {
+	return stateName[ss]
+}
+
+func main() {
+	og := StateIdle
+	fmt.Println("Original:", og)
+	ns := transition(StateIdle)
+	fmt.Println("Transition to:", ns)
+
+	ns2 := transition(ns)
+	fmt.Println("Transition Back:", ns2)
+
+}
+
+func transition(s ServerState) ServerState {
+	switch s {
+	case StateIdle:
+		return StateConnected
+	case StateConnected, StateRetrying:
+		return StateIdle
+	case StateError:
+		return StateError
+	default:
+		panic(fmt.Errorf("unknown state: %s", s))
+	}
+
+}
---
+++ g-b-e/enums/go.mod
@@ -0,0 +1,3 @@
+module example.com/enums
+
+go 1.25.4
